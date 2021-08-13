bea9a5ce32b138e8a18dc851290b467635258496
Enamul Haque <enamul@apptunix.com>
2021-08-13 18:45:57 +0530

    service fee added and build updated

--- store/polyfills-es5.js
+++ store/polyfills-es5.js
@@ -20468,7 +20468,7 @@
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /home/apptunix/Downloads/Vegon-store/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /home/apptunix/Desktop/Enamul/JAD_STORE/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -20476,15 +20476,15 @@
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     __webpack_require__(
-    /*! /home/apptunix/Downloads/Vegon-store/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */
+    /*! /home/apptunix/Desktop/Enamul/JAD_STORE/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
     __webpack_require__(
-    /*! /home/apptunix/Downloads/Vegon-store/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js */
+    /*! /home/apptunix/Desktop/Enamul/JAD_STORE/mazat_store/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
 
     module.exports = __webpack_require__(
-    /*! /home/apptunix/Downloads/Vegon-store/mazat_store/src/polyfills.ts */
+    /*! /home/apptunix/Desktop/Enamul/JAD_STORE/mazat_store/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
